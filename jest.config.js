c12641ffaf310f00deb7c620f134c6035593f6df
Nomura Suzume <suzume315@g00.g1e.org>
2021-06-03 20:29:34 +0900

    updated jest config not to generate coverage files but for lcov

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,7 @@ const {defaults} = require('jest-config')
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  coverageReporters: ["lcov"],
   moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts']
 };
