c849c878d4f8dd42d7de94b9105833d7ea9aef80
Nomura Suzume <suzume315@g00.g1e.org>
2021-06-04 00:34:32 +0900

    fixed sample script to handle the promise and resolve it.

--- fixtures/node/run.js
+++ fixtures/node/run.js
@@ -5,5 +5,7 @@ let f = new F0()
 f.setHost("http://htb.testing.local")
 f.get("/get").hasStatus(200).then(()=>{
     console.log("status 200 ok")
+}).catch(()=>{
+    console.log("status not ok")
 })
 
